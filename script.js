0c68c2dd7d4d7374bfdd7dc243aed343bf4d699b
Maria Lisboa <marialisboa1515@gmail.com>
2021-04-22 22:58:17 -0300

    comentei arquivo js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createProductImageElement(imageSource) {
   img.src = imageSource;
   return img;
 }
-
+// função que cria e adiciona valores e atributos aos elementos para adicionar na section abaixo.
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -12,6 +12,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Cria as sections para cada item que virá da requisição da API - 
+// especificando os elementos que estaram dentro da section.
 function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -22,21 +24,25 @@ function createProductItemElement({ id, title, thumbnail }) {
   return section;
 }
 
+// função que cria a lista de produtos da requisição da API 
 const createList = (data) => {
-   const dados = data.forEach((element) => {
+   data.forEach((element) => {
    const section = document.querySelectorAll('.items')[0];
    section.appendChild(createProductItemElement(element));
    return section;
   });
-  return dados;
+  
+  // return dados;
 };
 
+// faço a requisição para a API do mercado livre como promise
 const fetchDataList = () => new Promise((resolve, reject) => {  
     fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => {
       response.json().then((data) => {
         resolve(data.results);
         console.log(data.results);
+        // remove o nome fixo que foi inserido no html ao finalizar a requisição- (loading)
         document.getElementsByClassName('loading')[0].remove();
       })
         .catch((erro) => {
@@ -44,22 +50,23 @@ const fetchDataList = () => new Promise((resolve, reject) => {
         });
     });
 });
-
+// Crio referência  a classe a qual irei manipular no localStorage
 const itens = () => document.querySelectorAll('.cart__items');
-
+// Adiciono os itens ao localStorage utilizando setItem
 const itensStorage = () => {
   itens().forEach((element) => {
   localStorage.setItem('CartItens', element.innerHTML);
   });
 };
-
+// Função de remover clicando apenas no item
 function cartItemClickListener(event) {
   console.log(event);
   const clicar = event.target;
   clicar.remove();
   itensStorage();
 }
-
+// função que cria a lista de itens que são adicionados ao carrinho após o click no botão
+// adicionar ao carrinho.
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -67,13 +74,15 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-
+// função de evento click para ser utilizado na função que removerá itens do
+// localStorage
 const revomeItensStorage = () => {
   itens().forEach((element) => {
   element.addEventListener('click', cartItemClickListener);
   });
 };
-
+// função que mantém os dados salvos no localStorage mesmo ao recarregar
+// a página. Utilizando o getItem
 const loadStorage = () => {
   itens().forEach((element) => {
   const item = element;
@@ -81,7 +90,7 @@ const loadStorage = () => {
   });
   revomeItensStorage();
 };
-
+// função que adiciona o item clicado ao carrinho (pelo Id)
 const fetchAdicionar = (itemID) => new Promise((resolve, reject) => {
   fetch(`https://api.mercadolibre.com/items/${itemID}`)
   .then((response) => {
@@ -99,25 +108,26 @@ const fetchAdicionar = (itemID) => new Promise((resolve, reject) => {
     });
   });
  });
-
+// função que remove item do localStorage
  function excluirItem(element) {
   const clicar = element;
   clicar.remove();
   itensStorage();
 }
-
+// função que remove todos os itens do carrinho
  function remove(e) {
   const cartItem = e.target.parentNode.querySelectorAll('.cart__item');
   cartItem.forEach((element) => {
   excluirItem(element);
 });
 }
-  
+ 
+  // evento de click para remover todo os itens do carrinho
  const removeTodosItens = () => {
   const buttonRemove = document.getElementsByClassName('empty-cart')[0];
   buttonRemove.addEventListener('click', remove);
 };
-
+// função de evento de click que adiciona pelo id -sku
 function addEvento() {
   const buttonElement = document.getElementsByClassName('item__add');
    for (let index = 0; index < buttonElement.length; index += 1) {
@@ -128,14 +138,14 @@ function addEvento() {
     });
     }
   }
-
+// função que recebe as funções que são assicronas
 const assicronas = async () => {
   createList(await fetchDataList());
   await addEvento();
   await loadStorage();
   await removeTodosItens();
 };
-
+// função que recebe funções quqe devem ser executadas ao abrir a página
 window.onload = function onload() { 
   assicronas();
 };
